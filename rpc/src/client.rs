6a85fa9a8feb1bf389ef109a5fe4d9348f90439d
Maciej Zwoliński <mac.zwolinski@gmail.com>
2023-07-03 15:14:05 +0200

    Simplify headers creation
    Co-authored-by: Yiannis Marangos <yiannis@eiger.co>

--- rpc/src/client.rs
+++ rpc/src/client.rs
@@ -2,13 +2,12 @@ use http::{header, HeaderValue};
 use jsonrpsee::http_client::{HeaderMap, HttpClient, HttpClientBuilder};
 
 pub fn new_http(conn_str: &str, auth_token: Option<&str>) -> crate::Result<HttpClient> {
-    let headers: HeaderMap<HeaderValue> = auth_token
-        .into_iter()
-        .map(|token| {
-            HeaderValue::from_str(&format!("Bearer {token}"))
-                .map(|value| (header::AUTHORIZATION, value))
-        })
-        .collect::<Result<_, _>>()?;
+    let mut headers = HeaderMap::new();
+
+    if let Some(token) = auth_token {
+        let val = HeaderValue::from_str(&format!("Bearer {token}"))?;
+        headers.insert(header::AUTHORIZATION, val);
+    }
 
     let client = HttpClientBuilder::default()
         .set_headers(headers)
