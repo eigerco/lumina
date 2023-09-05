e05be0f75cf97d24c2371502b1dea961ac0ffde1
Yiannis Marangos <yiannis@eiger.co>
2023-09-05 19:13:16 +0300

    fix: make celestia-rpc to compile in wasm32 target

--- rpc/src/client.rs
+++ rpc/src/client.rs
@@ -1,36 +1,45 @@
-use http::{header, HeaderValue};
-use jsonrpsee::http_client::{HeaderMap, HttpClient, HttpClientBuilder};
-use jsonrpsee::ws_client::{WsClient, WsClientBuilder};
+pub use self::imp::*;
 
-use crate::Result;
+#[cfg(not(target_arch = "wasm32"))]
+mod imp {
+    use crate::Result;
+    use http::{header, HeaderValue};
+    use jsonrpsee::http_client::{HeaderMap, HttpClient, HttpClientBuilder};
+    use jsonrpsee::ws_client::{WsClient, WsClientBuilder};
 
-pub fn new_http(conn_str: &str, auth_token: Option<&str>) -> Result<HttpClient> {
-    let mut headers = HeaderMap::new();
+    pub fn new_http(conn_str: &str, auth_token: Option<&str>) -> Result<HttpClient> {
+        let mut headers = HeaderMap::new();
 
-    if let Some(token) = auth_token {
-        let val = HeaderValue::from_str(&format!("Bearer {token}"))?;
-        headers.insert(header::AUTHORIZATION, val);
+        if let Some(token) = auth_token {
+            let val = HeaderValue::from_str(&format!("Bearer {token}"))?;
+            headers.insert(header::AUTHORIZATION, val);
+        }
+
+        let client = HttpClientBuilder::default()
+            .set_headers(headers)
+            .build(conn_str)?;
+
+        Ok(client)
     }
 
-    let client = HttpClientBuilder::default()
-        .set_headers(headers)
-        .build(conn_str)?;
+    pub async fn new_websocket(conn_str: &str, auth_token: Option<&str>) -> Result<WsClient> {
+        let mut headers = HeaderMap::new();
 
-    Ok(client)
+        if let Some(token) = auth_token {
+            let val = HeaderValue::from_str(&format!("Bearer {token}"))?;
+            headers.insert(header::AUTHORIZATION, val);
+        }
+
+        let client = WsClientBuilder::default()
+            .set_headers(headers)
+            .build(conn_str)
+            .await?;
+
+        Ok(client)
+    }
 }
 
-pub async fn new_websocket(conn_str: &str, auth_token: Option<&str>) -> Result<WsClient> {
-    let mut headers = HeaderMap::new();
-
-    if let Some(token) = auth_token {
-        let val = HeaderValue::from_str(&format!("Bearer {token}"))?;
-        headers.insert(header::AUTHORIZATION, val);
-    }
-
-    let client = WsClientBuilder::default()
-        .set_headers(headers)
-        .build(conn_str)
-        .await?;
-
-    Ok(client)
+#[cfg(target_arch = "wasm32")]
+mod imp {
+    // TODO: implement HttpClient with `fetch`
 }
--- rpc/src/error.rs
+++ rpc/src/error.rs
@@ -1,11 +1,10 @@
-use http::header::InvalidHeaderValue;
-
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[cfg(not(target_arch = "wasm32"))]
     #[error("Token contains invalid characters: {0}")]
-    InvalidCharactersInToken(#[from] InvalidHeaderValue),
+    InvalidCharactersInToken(#[from] http::header::InvalidHeaderValue),
 
     #[error(transparent)]
     JsonRpc(#[from] jsonrpsee::core::Error),
