6ffe648e1f931ff84fc759de39ff02f92d06ec78
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2023-09-19 11:42:47 +0200

    cleanup

--- node/src/exchange/utils.rs
+++ node/src/exchange/utils.rs
@@ -58,54 +58,6 @@ impl HeaderResponseExt for HeaderResponse {
     }
 }
 
-/*
-fn gen_extended_header(height: u64) -> ExtendedHeader {
-    RawExtendedHeader {
-        header: Some(
-            Header {
-                version: Version { block: 11, app: 1 },
-                chain_id: "private".to_string().try_into().unwrap(),
-                height: height.try_into().unwrap(),
-                time: Time::now(),
-                last_block_id: None,
-                last_commit_hash: Hash::default(),
-                data_hash: Hash::default(),
-                validators_hash: Hash::default(),
-                next_validators_hash: Hash::default(),
-                consensus_hash: Hash::default(),
-                app_hash: AppHash::default(),
-                last_results_hash: Hash::default(),
-                evidence_hash: Hash::default(),
-                proposer_address: tendermint::account::Id::new([0; 20]),
-            }
-            .into(),
-        ),
-        commit: Some(
-            Commit {
-                height: height.try_into().unwrap(),
-                block_id: tendermint::block::Id {
-                    hash: Hash::Sha256(rand::random()),
-                    ..Default::default()
-                },
-                ..Default::default()
-            }
-            .into(),
-        ),
-        validator_set: Some(ValidatorSet::new(Vec::new(), None).into()),
-        dah: Some(
-            DataAvailabilityHeader {
-                row_roots: Vec::new(),
-                column_roots: Vec::new(),
-                hash: [0; 32],
-            }
-            .into(),
-        ),
-    }
-    .try_into()
-    .unwrap()
-}
-*/
-
 pub(super) trait ExtendedHeaderExt {
     fn to_header_response(&self) -> HeaderResponse;
 }
