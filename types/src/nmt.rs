02a82d89eef72ee3a1b1b6f8ffe8bafd666845a3
Maciej Zwoliński <mac.zwolinski@gmail.com>
2023-09-22 12:15:30 +0200

    feat(types): expose commitment creation for shares (#78)

--- types/src/nmt.rs
+++ types/src/nmt.rs
@@ -7,7 +7,7 @@ mod namespace_proof;
 mod namespaced_hash;
 
 pub use self::namespace_proof::NamespaceProof;
-pub use self::namespaced_hash::{NamespacedHash, NamespacedHashExt};
+pub use self::namespaced_hash::{NamespacedHash, NamespacedHashExt, RawNamespacedHash};
 use crate::{Error, Result};
 
 pub const NS_VER_SIZE: usize = 1;
