0bd2b7e560de5e8fe451ed9cc33238ddafe5c4c5
Maciej Zwoliński <mac.zwolinski@gmail.com>
2023-10-11 17:04:32 +0200

    chore: update celestia node to 0.11.0-rc15

--- rpc/src/share.rs
+++ rpc/src/share.rs
@@ -1,21 +1,18 @@
 use celestia_types::nmt::Namespace;
-use celestia_types::{DataAvailabilityHeader, ExtendedDataSquare, NamespacedShares, Share};
+use celestia_types::{ExtendedDataSquare, ExtendedHeader, NamespacedShares, Share};
 use jsonrpsee::proc_macros::rpc;
 
 #[rpc(client)]
 pub trait Share {
     /// GetEDS gets the full EDS identified by the given root.
     #[method(name = "share.GetEDS")]
-    async fn share_get_eds(
-        &self,
-        root: &DataAvailabilityHeader,
-    ) -> Result<ExtendedDataSquare, Error>;
+    async fn share_get_eds(&self, root: &ExtendedHeader) -> Result<ExtendedDataSquare, Error>;
 
     /// GetShare gets a Share by coordinates in EDS.
     #[method(name = "share.GetShare")]
     async fn share_get_share(
         &self,
-        root: &DataAvailabilityHeader,
+        root: &ExtendedHeader,
         row: u64,
         col: u64,
     ) -> Result<Share, Error>;
@@ -24,15 +21,11 @@ pub trait Share {
     #[method(name = "share.GetSharesByNamespace")]
     async fn share_get_shares_by_namespace(
         &self,
-        root: &DataAvailabilityHeader,
+        root: &ExtendedHeader,
         namespace: Namespace,
     ) -> Result<NamespacedShares, Error>;
 
-    /// ProbabilityOfAvailability calculates the probability of the data square being available based on the number of samples collected.
-    #[method(name = "share.ProbabilityOfAvailability")]
-    async fn share_probability_of_availability(&self) -> Result<f64, Error>;
-
     /// SharesAvailable subjectively validates if Shares committed to the given Root are available on the Network.
     #[method(name = "share.SharesAvailable")]
-    async fn share_shares_available(&self, root: &DataAvailabilityHeader) -> Result<(), Error>;
+    async fn share_shares_available(&self, root: &ExtendedHeader) -> Result<(), Error>;
 }
