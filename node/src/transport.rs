0befffc46f4f43008faab91340b0ec6b3d5315df
Yiannis Marangos <yiannis@eiger.co>
2023-10-25 15:30:47 +0300

    add more comments

--- node/src/transport.rs
+++ node/src/transport.rs
@@ -34,9 +34,14 @@ mod imp {
             //
             // This means that machine needs 8gb of ram to handle 8192
             // streams (the default). For this reason we lower max streams
-            // to 2048.
+            // to 2048, so the maximum memory usage will be 2gb.
             //
             // More info: https://github.com/libp2p/rust-yamux/issues/162
+            //
+            // NOTE: go-libp2p sets 16mb for receive window, but they have
+            // connection and memory limits in a higher layer. rust-libp2p
+            // doesn't implement this, and if we used 16mb here we would be
+            // vulnerable to DoS attacks.
             yamux_config.set_receive_window_size(1024 * 1024);
             yamux_config.set_max_buffer_size(1024 * 1024);
             yamux_config.set_max_num_streams(2048);
