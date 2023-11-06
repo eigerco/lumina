b42aa4585a9bd906904d7fa3a122c4df749b16b3
Maciej Zwoliński <mac.zwolinski@gmail.com>
2023-11-06 12:41:17 +0100

    feat: add support for celestia mainnet (#124)

--- celestia/src/native.rs
+++ celestia/src/native.rs
@@ -34,9 +34,10 @@ struct Args {
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, ValueEnum)]
 enum Network {
+    #[default]
+    Mainnet,
     Arabica,
     Mocha,
-    #[default]
     Private,
 }
 
@@ -87,6 +88,7 @@ pub async fn run() -> Result<()> {
 
 fn network_id(network: Network) -> &'static str {
     match network {
+        Network::Mainnet => "celestia",
         Network::Arabica => "arabica-10",
         Network::Mocha => "mocha-4",
         Network::Private => "private",
@@ -95,6 +97,7 @@ fn network_id(network: Network) -> &'static str {
 
 fn network_genesis(network: Network) -> Result<Option<Hash>> {
     let hex = match network {
+        Network::Mainnet => "6BE39EFD10BA412A9DB5288488303F5DD32CF386707A5BEF33617F4C43301872",
         Network::Arabica => "5904E55478BA4B3002EE885621E007A2A6A2399662841912219AECD5D5CBE393",
         Network::Mocha => "B93BBE20A0FBFDF955811B6420F8433904664D45DB4BF51022BE4200C1A1680D",
         Network::Private => return Ok(None),
@@ -111,6 +114,22 @@ fn network_genesis(network: Network) -> Result<Option<Hash>> {
 
 async fn network_bootnodes(network: Network) -> Result<Vec<Multiaddr>> {
     match network {
+        Network::Mainnet => Ok(
+            [
+                "/dns4/da-bridge-1.celestia-bootstrap.net/tcp/2121/p2p/12D3KooWSqZaLcn5Guypo2mrHr297YPJnV8KMEMXNjs3qAS8msw8",
+                "/dns4/da-bridge-2.celestia-bootstrap.net/tcp/2121/p2p/12D3KooWQpuTFELgsUypqp9N4a1rKBccmrmQVY8Em9yhqppTJcXf",
+                "/dns4/da-bridge-3.celestia-bootstrap.net/tcp/2121/p2p/12D3KooWSGa4huD6ts816navn7KFYiStBiy5LrBQH1HuEahk4TzQ",
+                "/dns4/da-bridge-4.celestia-bootstrap.net/tcp/2121/p2p/12D3KooWHBXCmXaUNat6ooynXG837JXPsZpSTeSzZx6DpgNatMmR",
+                "/dns4/da-bridge-5.celestia-bootstrap.net/tcp/2121/p2p/12D3KooWDGTBK1a2Ru1qmnnRwP6Dmc44Zpsxi3xbgFk7ATEPfmEU",
+                "/dns4/da-bridge-6.celestia-bootstrap.net/tcp/2121/p2p/12D3KooWLTUFyf3QEGqYkHWQS2yCtuUcL78vnKBdXU5gABM1YDeH",
+                "/dns4/da-full-1.celestia-bootstrap.net/tcp/2121/p2p/12D3KooWKZCMcwGCYbL18iuw3YVpAZoyb1VBGbx9Kapsjw3soZgr",
+                "/dns4/da-full-2.celestia-bootstrap.net/tcp/2121/p2p/12D3KooWE3fmRtHgfk9DCuQFfY3H3JYEnTU3xZozv1Xmo8KWrWbK",
+                "/dns4/da-full-3.celestia-bootstrap.net/tcp/2121/p2p/12D3KooWK6Ftsd4XsWCsQZgZPNhTrE5urwmkoo5P61tGvnKmNVyv",
+            ]
+            .iter()
+            .map(|s| s.parse().unwrap())
+            .collect()
+        ),
         Network::Arabica => Ok(
             [
                 "/dns4/da-bridge.celestia-arabica-10.com/tcp/2121/p2p/12D3KooWM3e9MWtyc8GkP8QRt74Riu17QuhGfZMytB2vq5NwkWAu",
