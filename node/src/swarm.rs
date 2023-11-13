ff6d652dd82d7bd39d96c701967dce8aff9890b9
Yiannis Marangos <yiannis@eiger.co>
2023-11-13 10:56:19 +0200

    fix: Use pre-defined DNS nameservers

--- node/src/swarm.rs
+++ node/src/swarm.rs
@@ -8,7 +8,7 @@ pub(crate) use self::imp::new_swarm;
 #[cfg(not(target_arch = "wasm32"))]
 mod imp {
     use super::*;
-    use libp2p::{noise, tcp, yamux};
+    use libp2p::{dns, noise, tcp, yamux};
 
     pub(crate) fn new_swarm<B>(keypair: Keypair, behaviour: B) -> Result<Swarm<B>, P2pError>
     where
@@ -38,8 +38,23 @@ mod imp {
                 config
             })?
             .with_quic()
-            .with_dns()
-            .map_err(P2pError::InitDns)?
+            // We do not use system's DNS because libp2p loads DNS servers only when
+            // `Swarm` get constructed. This is not a problem for server machines, but
+            // it is for movable machines such as laptops and smart phones. Because of
+            // that, the following edge cases can happen:
+            //
+            // 1. Machine connects to WiFi A. Our node starts and uses DNS that
+            //    WiFi A announced. Then machine moves to WiFi B, the old DNS servers
+            //    become unreachable, but libp2p still uses the old ones.
+            // 2. Machine is not connected to the Internet. Our node starts and does not
+            //    find any DNS servers defined. Then machine connects to the Internet, but
+            //    libp2p still does not have any DNS nameservers defined.
+            //
+            // By having a pre-defined public servers, these edge cases solved.
+            .with_dns_config(
+                dns::ResolverConfig::cloudflare(),
+                dns::ResolverOpts::default(),
+            )
             .with_behaviour(|_| behaviour)
             .expect("Moving behaviour doesn't fail")
             .with_swarm_config(|config| {
