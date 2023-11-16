fa71b979bb0da0765793effd8cb692047741a5de
Maciej Zwoliński <mac.zwolinski@gmail.com>
2023-11-16 14:09:00 +0100

    feat: Bootstrap more aggresively when too few peers

--- node/src/p2p.rs
+++ node/src/p2p.rs
@@ -7,6 +7,7 @@ use celestia_proto::p2p::pb::{header_request, HeaderRequest};
 use celestia_types::hash::Hash;
 use celestia_types::ExtendedHeader;
 use futures::StreamExt;
+use instant::Instant;
 use libp2p::{
     autonat,
     core::{ConnectedPoint, Endpoint},
@@ -36,6 +37,18 @@ use crate::utils::{
 };
 
 pub use crate::header_ex::HeaderExError;
+
+// Minimal number of peers that we want to maintain connection to.
+// If we have fewer peers than that, we will try to reconnect / discover
+// more aggresively.
+const MIN_CONNECTED_PEERS: u64 = 4;
+// Bootstrap procedure is a bit misleading as a name. It is actually
+// scanning the network thought the already known peers and find new
+// ones. It also recovers connectivity of previously known peers and
+// refreshes the routing table.
+//
+// libp2p team suggests to start bootstrap procedure every 5 minute
+const KADEMLIA_BOOTSTRAP_PERIOD: Duration = Duration::from_secs(5 * 60);
 
 type Result<T, E = P2pError> = std::result::Result<T, E>;
 
@@ -415,7 +428,8 @@ where
 
     async fn run(&mut self) {
         let mut report_interval = Interval::new(Duration::from_secs(60)).await;
-        let mut kademlia_interval = Interval::new(Duration::from_secs(5 * 60)).await;
+        let mut kademlia_interval = Interval::new(Duration::from_secs(30)).await;
+        let mut kademlia_last_bootstrap = Instant::now();
 
         // Initiate discovery
         let _ = self.swarm.behaviour_mut().kademlia.bootstrap();
@@ -426,13 +440,13 @@ where
                     self.report();
                 }
                 _ = kademlia_interval.tick() => {
-                    // Bootstrap procedure is a bit misleading as a name. It is actually
-                    // scanning the network thought the already known peers and find new
-                    // ones. It also recovers connectivity of previously known peers and
-                    // refreshes the routing table.
-                    //
-                    // libp2p team suggests to start bootstrap procedure every 5 minute.
-                    let _ = self.swarm.behaviour_mut().kademlia.bootstrap();
+                    if self.peer_tracker.info().num_connected_peers < MIN_CONNECTED_PEERS
+                        || kademlia_last_bootstrap.elapsed() > KADEMLIA_BOOTSTRAP_PERIOD
+                    {
+                        info!("Running kademlia bootstrap procedure.");
+                        let _ = self.swarm.behaviour_mut().kademlia.bootstrap();
+                        kademlia_last_bootstrap = Instant::now();
+                    }
                 }
                 ev = self.swarm.select_next_some() => {
                     if let Err(e) = self.on_swarm_event(ev).await {
