89b35ec5d50b20f16e63c954ebcceaf8ec43bd42
Denis Deniz <denis2.glez@gmail.com>
2023-11-20 18:14:13 +0100

    fix: use Docker Compose v2 consistently
    From July 2023 Compose v1 stopped receiving updates.

--- tools/gen_auth_tokens.sh
+++ tools/gen_auth_tokens.sh
@@ -41,7 +41,7 @@ ensure_dotenv_file() {
 
 generate_token() {
   local auth_level="$1"
-  docker-compose -f "$DOCKER_COMPOSE_FILE" exec -T bridge-0 \
+  docker compose -f "$DOCKER_COMPOSE_FILE" exec -T bridge-0 \
     celestia bridge auth "$auth_level" --p2p.network private
 }
 
