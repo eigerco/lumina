15bb11ed64cb549e8bf0ef99dc92c374958f376f
Maciej Zwoliński <mac.zwolinski@gmail.com>
2024-02-24 15:59:35 +0100

    apply in memory blockstore suggestion

--- blockstore/src/in_memory_blockstore.rs
+++ blockstore/src/in_memory_blockstore.rs
@@ -1,5 +1,5 @@
 use cid::CidGeneric;
-use dashmap::{mapref::entry::Entry, DashMap};
+use dashmap::DashMap;
 
 use crate::{convert_cid, Blockstore, Result};
 
@@ -21,10 +21,7 @@ impl<const MAX_MULTIHASH_SIZE: usize> InMemoryBlockstore<MAX_MULTIHASH_SIZE> {
     }
 
     fn insert_cid(&self, cid: CidGeneric<MAX_MULTIHASH_SIZE>, data: &[u8]) -> Result<()> {
-        let cid_entry = self.map.entry(cid);
-        if matches!(cid_entry, Entry::Vacant(_)) {
-            cid_entry.insert(data.to_vec());
-        }
+        self.map.entry(cid).or_insert_with(|| data.to_vec());
 
         Ok(())
     }
