3c624ef87f193a5302fff7036fcf145eb9b328a2
Maciej Zwoliński <mac.zwolinski@gmail.com>
2024-02-27 18:55:22 +0100

    broken docs links

--- types/src/nmt/namespaced_merkle_tree.rs
+++ types/src/nmt/namespaced_merkle_tree.rs
@@ -5,8 +5,12 @@ use super::{NamespacedHash, NS_SIZE};
 pub use nmt_rs::simple_merkle::tree::MerkleHash;
 
 /// Hasher for generating [`Namespace`] aware hashes.
+///
+/// [`Namespace`] crate::nmt::Namespace
 pub type NamespacedSha2Hasher = nmt_rs::NamespacedSha2Hasher<NS_SIZE>;
 /// [`Namespace`] aware merkle tree.
+///
+/// [`Namespace`] crate::nmt::Namespace
 pub type Nmt = nmt_rs::NamespaceMerkleTree<MemDb<NamespacedHash>, NamespacedSha2Hasher, NS_SIZE>;
 
 /// An extention trait for the [`NamespacedHash`] to perform additional actions.
