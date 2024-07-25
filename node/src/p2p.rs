c99503eef9b27779c34892ec4de35ee1d5a27632
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2024-07-25 17:27:57 +0200

    feat: Header pruning

--- node/src/p2p.rs
+++ node/src/p2p.rs
@@ -193,7 +193,7 @@ where
     /// List of the addresses on which to listen for incoming connections.
     pub listen_on: Vec<Multiaddr>,
     /// The store for headers.
-    pub blockstore: B,
+    pub blockstore: Arc<B>,
     /// The store for headers.
     pub store: Arc<S>,
     /// Event publisher.
@@ -624,7 +624,11 @@ where
         let gossipsub = init_gossipsub(&args, [&header_sub_topic, &bad_encoding_fraud_sub_topic])?;
 
         let kademlia = init_kademlia(&args)?;
-        let bitswap = init_bitswap(args.blockstore, args.store.clone(), &args.network_id)?;
+        let bitswap = init_bitswap(
+            args.blockstore.clone(),
+            args.store.clone(),
+            &args.network_id,
+        )?;
 
         let header_ex = HeaderExBehaviour::new(HeaderExConfig {
             network_id: &args.network_id,
@@ -1200,7 +1204,7 @@ where
 }
 
 fn init_bitswap<B, S>(
-    blockstore: B,
+    blockstore: Arc<B>,
     store: Arc<S>,
     network_id: &str,
 ) -> Result<beetswap::Behaviour<MAX_MH_SIZE, B>>
