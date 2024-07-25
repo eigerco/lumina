7509ee98634dd2f0e048224e964bb1b7209d6812
Yiannis Marangos <psyberbits@gmail.com>
2024-07-25 22:04:44 +0300

    Update node/src/p2p.rs
    Co-authored-by: Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
    Signed-off-by: Yiannis Marangos <psyberbits@gmail.com>

--- node/src/p2p.rs
+++ node/src/p2p.rs
@@ -719,8 +719,8 @@ where
         for (peer_id, addrs) in &self.bootnodes {
             let dial_opts = DialOpts::peer_id(*peer_id)
                 .addresses(addrs.clone())
-                // This is needed when addresses are provided otherwise
-                // our `kademlia::Behaviour` wrapper will not cononicalize them.
+                // This is needed when addresses are provided in  otherwise
+                // our `kademlia::Behaviour` wrapper will not canonicalize them.
                 .extend_addresses_through_behaviour()
                 // Tell Swarm not to dial if peer is already connected or there
                 // is an ongoing dialing.
