427e108eef847054025e9c075235ed5f80d4af39
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2024-09-27 08:27:03 +0200

    wip2

--- node-wasm/src/client.rs
+++ node-wasm/src/client.rs
@@ -398,29 +398,71 @@ impl WasmNodeConfig {
 
 #[cfg(test)]
 mod tests {
-    use crate::worker::NodeWorker;
-
     use super::*;
+    use crate::worker::NodeWorker;
+    use celestia_rpc::{prelude::*, Client};
+    use celestia_types::ExtendedHeader;
+    use libp2p::{multiaddr::Protocol, Multiaddr};
+    use serde_wasm_bindgen::from_value;
+    use tracing_subscriber::filter::LevelFilter;
+    use tracing_subscriber::{
+        fmt::time::UtcTime, layer::SubscriberExt, util::SubscriberInitExt, Layer,
+    };
+    use tracing_web::MakeConsoleWriter;
     use wasm_bindgen_futures::spawn_local;
     use wasm_bindgen_test::wasm_bindgen_test;
     use web_sys::MessageChannel;
 
-use tracing_subscriber::{fmt::time::UtcTime, layer::SubscriberExt, util::SubscriberInitExt, Layer};
-use tracing_web::MakeConsoleWriter;
-use tracing_subscriber::filter::LevelFilter;
+    const WS_URL: &str = "ws://localhost:36658";
 
-    #[wasm_bindgen_test]
-    async fn test_connect() {
+    pub async fn fetch_bridge_info(client: &Client) -> Multiaddr {
+        //let client = Client::new(WS_URL).await.unwrap();
+        let bridge_info = client.p2p_info().await.unwrap();
+
+        let mut ma = bridge_info
+            .addrs
+            .into_iter()
+            .find(|ma| {
+                let not_localhost = !ma
+                    .iter()
+                    .any(|prot| prot == Protocol::Ip4("127.0.0.1".parse().unwrap()));
+                let webtransport = ma
+                    .protocol_stack()
+                    .any(|protocol| protocol == "webtransport");
+                not_localhost && webtransport
+            })
+            .expect("Bridge doesn't listen on webtransport");
+
+        if !ma.protocol_stack().any(|protocol| protocol == "p2p") {
+            ma.push(Protocol::P2p(bridge_info.id.into()))
+        }
+
+        tracing::info!("{ma:?}");
+
+        ma
+    }
+
+    fn setup_logs() {
         let fmt_layer = tracing_subscriber::fmt::layer()
             .with_ansi(false)
             .with_timer(UtcTime::rfc_3339()) // std::time is not available in browsers
             .with_writer(MakeConsoleWriter) // write events to the console
-            .with_filter(LevelFilter::INFO); // TODO: allow customizing the log level
+            .with_filter(LevelFilter::TRACE); // TODO: allow customizing the log level
         tracing_subscriber::registry().with(fmt_layer).init();
+    }
+
+    #[wasm_bindgen_test]
+    async fn connect_to_network() {
+        setup_logs();
+        let rpc_client = Client::new(WS_URL).await.unwrap();
 
         let message_channel = MessageChannel::new().unwrap();
         let mut worker = NodeWorker::new(message_channel.port1().into());
-        let client = NodeClient::new(message_channel.port2().into()).await.unwrap();
+        let client = NodeClient::new(message_channel.port2().into())
+            .await
+            .unwrap();
+
+        let ma = fetch_bridge_info(&rpc_client).await;
 
         spawn_local(async move {
             worker.run().await.unwrap();
@@ -428,20 +470,24 @@ use tracing_subscriber::filter::LevelFilter;
 
         assert!(!client.is_running().await.unwrap());
 
-        let config = WasmNodeConfig { 
-            network: Network::Private,
-            bootnodes: vec![
-            "/ip4/172.21.0.3/udp/2121/quic-v1/webtransport/certhash/uEiBR2loOqapeUYGWCLReNhXRwe0voP_2tXZdtVz9NU9qLQ/certhash/uEiAx4Mk68KLC1RPqldMev6fqZSxcwqUULBjBHMvGqqRYGw/p2p/12D3KooWQynhR9sCTqBcjA73JNzV6mNbfqz4MhCj7gKj5VFAiEFH"
-            .to_string()]
-        };
+        client
+            .start(WasmNodeConfig {
+                network: Network::Private,
+                bootnodes: vec![format!("{ma}")],
+            })
+            .await
+            .unwrap();
 
-        client.start(config).await.unwrap();
         assert!(client.is_running().await.unwrap());
 
         client.wait_connected_trusted().await.unwrap();
 
         let info = client.network_info().await.unwrap();
         assert_eq!(info.num_peers, 1);
-    }
 
+        let bridge_head_header = rpc_client.header_network_head().await.unwrap();
+        let head_header: ExtendedHeader =
+            from_value(client.request_head_header().await.unwrap()).unwrap();
+        assert_eq!(head_header, bridge_head_header)
+    }
 }
