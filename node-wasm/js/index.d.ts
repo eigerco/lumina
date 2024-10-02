92c77823e2fed73dd71f12fa7bd7177ca7ff2098
zvolin <mac.zwolinski@gmail.com>
2024-10-02 15:45:16 +0200

    fix, run tsc on main

--- node-wasm/js/index.d.ts
+++ node-wasm/js/index.d.ts
@@ -3,4 +3,6 @@
 */
 export function spawnNode(): Promise<NodeClient>;
 export * from "lumina-node-wasm";
+export default init;
 import { NodeClient } from "lumina-node-wasm";
+import init from "lumina-node-wasm";
