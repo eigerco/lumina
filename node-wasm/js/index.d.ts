6fee3eddfed1148bb1c129c3b9e98a891ec1dfd2
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2024-10-03 12:57:55 +0200

    chore(node-wasm): Switch to bundler target for wasm-pack for manifest v3 (#398)
    Signed-off-by: Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
    Co-authored-by: Maciej Zwoliński <mac.zwolinski@gmail.com>

--- node-wasm/js/index.d.ts
+++ node-wasm/js/index.d.ts
@@ -3,4 +3,3 @@
 */
 export function spawnNode(): Promise<NodeClient>;
 export * from "lumina-node-wasm";
-export default function init(): Promise<void>;
--- node-wasm/js/worker.js
+++ node-wasm/js/worker.js
@@ -1,10 +1,8 @@
-import init, { NodeWorker } from "lumina-node-wasm"
+import { NodeWorker } from "lumina-node-wasm"
 
 Error.stackTraceLimit = 99;
 
-init().then(async () => {
-  let worker = new NodeWorker(self);
-  console.log("Starting NodeWorker");
+let worker = new NodeWorker(self);
+console.log("Starting NodeWorker");
 
-  await worker.run();
-});
+worker.run();
