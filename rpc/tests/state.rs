520b6788d9f24f87c725a0f18b238c69a77bf36e
Maciej Zwoliński <mac.zwolinski@gmail.com>
2024-10-09 10:48:01 +0200

    fix!(rpc): use correct blob type in state submit pay for blob (#418)

--- rpc/tests/state.rs
+++ rpc/tests/state.rs
@@ -1,6 +1,8 @@
 #![cfg(not(target_arch = "wasm32"))]
 
+use crate::utils::{random_bytes, random_ns};
 use celestia_rpc::prelude::*;
+use celestia_types::{Blob, TxConfig};
 
 pub mod utils;
 
@@ -28,4 +30,25 @@ async fn balance_for_address() {
 
     let balance = client.state_balance_for_address(&my_addr).await.unwrap();
     assert_eq!(my_balance, balance);
+}
+
+#[tokio::test]
+async fn submit_pay_for_blob() {
+    let client = new_test_client(AuthLevel::Write).await.unwrap();
+    let namespace = random_ns();
+    let data = random_bytes(5);
+    let blob = Blob::new(namespace, data).unwrap();
+
+    let tx_response = client
+        .state_submit_pay_for_blob(&[blob.clone().into()], TxConfig::default())
+        .await
+        .unwrap();
+
+    let received_blob = client
+        .blob_get(tx_response.height as u64, namespace, blob.commitment)
+        .await
+        .unwrap();
+
+    received_blob.validate().unwrap();
+    assert_eq!(received_blob.data, blob.data);
 }
