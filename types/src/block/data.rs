daedab66ee7d34c188f953892f037443a6d471d2
Yiannis Marangos <yiannis@eiger.co>
2024-11-26 15:58:01 +0200

    fix doc issues

--- types/src/block/data.rs
+++ types/src/block/data.rs
@@ -4,11 +4,21 @@ use tendermint_proto::Protobuf;
 
 use crate::Error;
 
+/// Data contained in a [`Block`].
+///
+/// [`Block`]: crate::block::Block
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 #[serde(try_from = "RawData", into = "RawData")]
 pub struct Data {
+    /// Transactions.
     pub txs: Vec<Vec<u8>>,
+
+    /// Square width of original data square.
     pub square_size: u64,
+
+    /// Hash is the root of a binary Merkle tree where the leaves of the tree are
+    /// the row and column roots of an extended data square. Hash is often referred
+    /// to as the "data root".
     pub hash: Vec<u8>,
 }
 
