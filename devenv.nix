823c138d3120c1285f06564728b38c1eb96011ed
zvolin <mac.zwolinski@gmail.com>
2025-02-05 17:49:33 +0100

    wip

--- devenv.nix
+++ devenv.nix
@@ -43,14 +43,13 @@ in
     swift
   ];
 
-  cachix.enable = false;
   dotenv.enable = true;
 
   languages.rust = {
     enable = true;
     channel = "stable";
     # Extra targets other than the native
-    targets = ["wasm32-unknown-unknown"];
+    targets = [ "wasm32-unknown-unknown" ];
   };
 
   languages.javascript = {
