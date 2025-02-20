1bfe7255ecc13210f50ee1d4d112bf791552f5ab
Diego <31937514+Ferret-san@users.noreply.github.com>
2025-02-20 17:06:03 +0100

    Update rpc/src/blobstream.rs
    Co-authored-by: Maciej Zwoliński <mac.zwolinski@gmail.com>
    Signed-off-by: Diego <31937514+Ferret-san@users.noreply.github.com>

--- rpc/src/blobstream.rs
+++ rpc/src/blobstream.rs
@@ -3,12 +3,12 @@ use celestia_types::MerkleProof;
 use jsonrpsee::proc_macros::rpc;
 use prost::bytes;
 
-// DataRootTupleRoot is the root of the merkle tree created
-// from a set of data root tuples.
+/// DataRootTupleRoot is the root of the merkle tree created
+/// from a set of data root tuples.
 pub type DataRootTupleRoot = Bytes;
 
-// DataRootTupleInclusionProof is the binary merkle
-// inclusion proof of a height to a data commitment.
+/// DataRootTupleInclusionProof is the binary merkle
+/// inclusion proof of a height to a data commitment.
 pub type DataRootTupleInclusionProof = MerkleProof;
 
 #[rpc(client)]
