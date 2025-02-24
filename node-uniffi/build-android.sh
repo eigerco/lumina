02c1285b092e76261551497086e4d49fbc9fd4a6
Maciej Zwoliński <mac.zwolinski@gmail.com>
2025-02-24 12:54:49 +0100

    chore(ci): add releasing for node-uniffi (#545)
    Signed-off-by: Maciej Zwoliński <mac.zwolinski@gmail.com>

--- node-uniffi/build-android.sh
+++ node-uniffi/build-android.sh
@@ -1,21 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+
+so_ext="so"
+if [ "$(uname)" == "Darwin" ]; then
+  so_ext="dylib"
+fi
 
 cargo build -p lumina-node-uniffi
 
-rustup target add \
-    aarch64-linux-android \
-    armv7-linux-androideabi \
-    x86_64-linux-android \
-    i686-linux-android
+cargo ndk \
+  -o ./app/src/main/jniLibs \
+  --manifest-path ./Cargo.toml \
+  -t armeabi-v7a \
+  -t arm64-v8a \
+  -t x86 \
+  -t x86_64 \
+  build --release
 
-cargo ndk -o ./app/src/main/jniLibs \
-        --manifest-path ./Cargo.toml \
-        -t armeabi-v7a \
-        -t arm64-v8a \
-        -t x86 \
-        -t x86_64 \
-        build --release
-
-cargo run --bin uniffi-bindgen generate --library ../target/debug/liblumina_node_uniffi.dylib --language kotlin --out-dir ./app/src/main/java/tech/forgen/lumina_node_uniffi/rust
-
-echo "Android build complete"
+cargo run --bin uniffi-bindgen \
+  generate \
+  --library ../target/debug/liblumina_node_uniffi.$so_ext \
+  --language kotlin \
+  --out-dir ./app/src/main/java/tech/forgen/lumina_node_uniffi/rust
