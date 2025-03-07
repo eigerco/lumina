94646656b1a13e8fb6fbc1f18829f385fded395d
Yakup Altay <yakup.altay@boun.edu.tr>
2025-03-07 04:26:21 +0300

    feat(rpc, types): enhance DAS module with detailed documentation and improved struct fields

--- rpc/src/das.rs
+++ rpc/src/das.rs
@@ -1,13 +1,14 @@
 use celestia_types::SamplingStats;
 use jsonrpsee::proc_macros::rpc;
 
+/// The `Das` RPC trait provides methods for interacting with Data Availability Sampling.
 #[rpc(client)]
 pub trait Das {
-  //// SamplingStats returns the current statistics over the DA sampling process.
-  #[method(name = "das.SamplingStats")]
-  async fn das_sampling_stats(&self) -> Result<SamplingStats, Error>;
+    /// Retrieves the current statistics over the DA sampling process.
+    #[method(name = "das.SamplingStats")]
+    async fn das_sampling_stats(&self) -> Result<SamplingStats, Error>;
 
-  //// WaitCatchUp blocks until DASer finishes catching up to the network head.
-  #[method(name = "das.WaitCatchUp")]
-  async fn das_wait_catch_up(&self) -> Result<(), Error>;
+    /// Blocks until DASer finishes catching up to the network head.
+    #[method(name = "das.WaitCatchUp")]
+    async fn das_wait_catch_up(&self) -> Result<(), Error>;
 }
--- rpc/tests/das.rs
+++ rpc/tests/das.rs
@@ -13,25 +13,22 @@ async fn das_sampling_stats() {
     let stats1 = client.das_sampling_stats().await.unwrap();
 
     client
-		.header_wait_for_height(stats1.network_head_height + 1)
-		.await
-		.unwrap();
+        .header_wait_for_height(stats1.network_head_height + 1)
+        .await
+        .unwrap();
 
     let stats2 = client.das_sampling_stats().await.unwrap();
 
-	assert!(stats2.head_of_sampled_chain >= stats1.head_of_sampled_chain);
+    assert!(stats2.head_of_sampled_chain >= stats1.head_of_sampled_chain);
 }
 
 #[tokio::test]
 async fn das_wait_catch_up() {
-	let client = new_test_client(AuthLevel::Read).await.unwrap();
+    let client = new_test_client(AuthLevel::Read).await.unwrap();
 
-	client
-		.das_wait_catch_up()
-		.await
-		.unwrap();
+    client.das_wait_catch_up().await.unwrap();
 
-	let stats = client.das_sampling_stats().await.unwrap();
+    let stats = client.das_sampling_stats().await.unwrap();
 
-	assert!(stats.catch_up_done);
+    assert!(stats.catch_up_done);
 }
