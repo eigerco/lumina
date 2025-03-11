f47f3c88a8d0e6c941af39e2b65bebee7a90014c
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2025-03-11 15:25:25 +0100

    feat: Multiplex message channel connection to worker

---
+++ node-wasm/src/ports/client.rs
@@ -0,0 +1,186 @@
+use std::collections::HashMap;
+use std::future::Future;
+
+use futures::future::FutureExt;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use tokio::select;
+use tokio::sync::mpsc;
+use tokio::sync::oneshot;
+use tokio_util::sync::{CancellationToken, DropGuard};
+use tracing::{error, warn};
+use wasm_bindgen::prelude::*;
+
+use crate::error::{Context, Error, Result};
+use crate::ports::common::{MessageId, MultiplexMessage, Port, Transferable};
+use lumina_node::executor::{spawn, JoinHandle};
+
+/// Client responsible for sending `Request`s and receiving `Response`s to them over a port like
+/// object.
+pub struct Client<Request, Response> {
+    request_tx: mpsc::UnboundedSender<(Request, Transferable, oneshot::Sender<Response>)>,
+    _worker_join_handle: JoinHandle,
+    _worker_drop_guard: DropGuard,
+}
+
+impl<Request, Response> Client<Request, Response>
+where
+    Request: Serialize + 'static,
+    Response: Serialize + DeserializeOwned + 'static,
+{
+    /// Create a new `Client` and start a background thread which forwards responses from the js
+    /// callback.
+    pub fn start(port: JsValue) -> Result<Client<Request, Response>> {
+        let cancellation_token = CancellationToken::new();
+        let (request_tx, request_rx) = mpsc::unbounded_channel();
+        let mut worker = ClientWorker::new(port, request_rx, cancellation_token.child_token())?;
+
+        let _worker_join_handle = spawn(async move {
+            if let Err(e) = worker.run().await {
+                error!("ClientWorker stopped because of a fatal error: {e}");
+            }
+        });
+
+        Ok(Client {
+            request_tx,
+            _worker_join_handle,
+            _worker_drop_guard: cancellation_token.drop_guard(),
+        })
+    }
+
+    /// Send a `Request` over the port and return a channel to receive `Response` over
+    pub fn send(
+        &self,
+        request: Request,
+        transferable: Option<JsValue>,
+    ) -> Result<impl Future<Output = Option<Response>>> {
+        let (tx, rx) = oneshot::channel();
+        self.request_tx
+            .send((request, transferable, tx))
+            .context("could not forward the request to ClientWorker")?;
+        Ok(rx.map(|r| r.ok()))
+    }
+}
+
+struct ClientWorker<Request, Response: Serialize> {
+    /// Port over which communication takes place
+    port: Port,
+    /// Queued responses from the onmessage callback
+    incoming_responses: mpsc::UnboundedReceiver<MultiplexMessage<Response>>,
+    /// Map of message ids waiting for response to oneshot channels to send the response over
+    pending_responses_map: HashMap<MessageId, oneshot::Sender<Response>>,
+    /// Queued requests to be sent
+    outgoing_requests: mpsc::UnboundedReceiver<(Request, Transferable, oneshot::Sender<Response>)>,
+    /// MessageId to be used for the next request
+    next_message_index: MessageId,
+    /// Cancellation token to stop the worker
+    cancellation_token: CancellationToken,
+}
+
+impl<Request, Response> ClientWorker<Request, Response>
+where
+    Request: Serialize,
+    Response: Serialize + DeserializeOwned + 'static,
+{
+    fn new(
+        port: JsValue,
+        request_tx: mpsc::UnboundedReceiver<(Request, Transferable, oneshot::Sender<Response>)>,
+        cancellation_token: CancellationToken,
+    ) -> Result<ClientWorker<Request, Response>> {
+        let (incoming_responses_tx, incoming_responses) = mpsc::unbounded_channel();
+
+        let port = Port::new_with_channels(port, incoming_responses_tx, None)?;
+
+        Ok(ClientWorker {
+            port,
+            incoming_responses,
+            outgoing_requests: request_tx,
+            next_message_index: Default::default(),
+            pending_responses_map: Default::default(),
+            cancellation_token,
+        })
+    }
+
+    async fn run(&mut self) -> Result<()> {
+        loop {
+            select! {
+                _ = self.cancellation_token.cancelled() => {
+                    return Ok(())
+                }
+                msg = self.incoming_responses.recv() => {
+                    let Some(MultiplexMessage {id, payload, }) = msg else {
+                        return Err(Error::new("Incoming message channel closed, should not happen"));
+                    };
+                    self.handle_incoming_response(id, payload);
+                }
+                request = self.outgoing_requests.recv() => {
+                    let Some((msg, transferable, response_tx)) = request else {
+                        return Err(Error::new("Outgoing requests channel closed, should not happen"));
+                    };
+                    self.handle_outgoing_request(msg, transferable, response_tx)?;
+                }
+            }
+        }
+    }
+
+    fn handle_incoming_response(&mut self, id: MessageId, payload: Option<Response>) {
+        let Some(response_sender) = self.pending_responses_map.remove(&id) else {
+            warn!("received unsolicited response for {id:?}, ignoring");
+            return;
+        };
+
+        // empty response means the responder on the server side was dropped, without
+        // sending a response, so let's do the same here
+        if let Some(response) = payload {
+            if response_sender.send(response).is_err() {
+                warn!("receiver for {id:?} dropped");
+            }
+        }
+    }
+
+    fn handle_outgoing_request(
+        &mut self,
+        request: Request,
+        transferable: Transferable,
+        response_tx: oneshot::Sender<Response>,
+    ) -> Result<()> {
+        let mid = self.next_message_index.post_increment();
+        let message = MultiplexMessage {
+            id: mid,
+            payload: Some(request),
+        };
+
+        if let Err(e) = self.send_message(message, transferable) {
+            error!("Failed to send request: {e}");
+            drop(response_tx);
+            return Ok(());
+        }
+
+        if self
+            .pending_responses_map
+            .insert(mid, response_tx)
+            .is_some()
+        {
+            return Err(Error::new("collision in message ids, should not happen"));
+        }
+
+        Ok(())
+    }
+
+    fn send_message(
+        &mut self,
+        message: MultiplexMessage<Request>,
+        transferable: Transferable,
+    ) -> Result<()> {
+        if let Some(transferable) = transferable {
+            self.port
+                .send_with_transferable(&message, transferable)
+                .context("failed to send outgoing request with transferable")?;
+        } else {
+            self.port
+                .send(&message)
+                .context("failed to send outgoing request without transferable")?;
+        }
+        Ok(())
+    }
+}
---
+++ node-wasm/src/ports/server.rs
@@ -0,0 +1,206 @@
+use std::marker::PhantomData;
+
+use futures::future::{FutureExt, LocalBoxFuture};
+use futures::stream::{FuturesUnordered, StreamExt};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use tokio::select;
+use tokio::sync::mpsc;
+use tokio::sync::oneshot;
+use tokio_util::sync::{CancellationToken, DropGuard};
+use tracing::{error, warn};
+use wasm_bindgen::prelude::*;
+
+use crate::error::{Context, Error, Result};
+use crate::ports::common::{MessageId, MultiplexMessage, Port};
+use lumina_node::executor::{spawn, JoinHandle};
+
+/// `Server` aggregates multiple existing [`ServerConnection`]s, receiving `Request`s
+/// from the connected clients, as well as handles new [`Client`]s connecting.
+pub struct Server<Request, Response> {
+    connections: Vec<ServerConnection<Request, Response>>,
+    requests_tx: mpsc::UnboundedSender<(Request, oneshot::Sender<Response>)>,
+    requests_rx: mpsc::UnboundedReceiver<(Request, oneshot::Sender<Response>)>,
+    ports_tx: mpsc::UnboundedSender<JsValue>,
+    ports_rx: mpsc::UnboundedReceiver<JsValue>,
+}
+
+impl<Request, Response> Server<Request, Response>
+where
+    Request: Serialize + DeserializeOwned + 'static,
+    Response: Serialize + 'static,
+{
+    /// Create a new `Server` without any client connections. See [`get_port_channel`] for adding a
+    /// new connection
+    pub fn new() -> Self {
+        let (requests_tx, requests_rx) = mpsc::unbounded_channel();
+        let (ports_tx, ports_rx) = mpsc::unbounded_channel();
+
+        Server {
+            connections: vec![],
+            requests_tx,
+            requests_rx,
+            ports_tx,
+            ports_rx,
+        }
+    }
+
+    /// Receive next `Request` coming from one of the connected clients. Should be called
+    /// semi-frequently, as it also transparently handles new clients connecting.
+    pub async fn recv(&mut self) -> Result<(Request, oneshot::Sender<Response>)> {
+        loop {
+            select! {
+                request = self.requests_rx.recv() => {
+                    return Ok(request.expect("request channel should not drop"))
+                }
+                port = self.ports_rx.recv() => {
+                    if let Err(e) = self.add_connection(port.expect("port channel should not drop")) {
+                        error!("Failed to add new client connection: {e}");
+                    }
+                }
+            }
+        }
+    }
+
+    fn add_connection(&mut self, port: JsValue) -> Result<()> {
+        let client_connection =
+            ServerConnection::start(port, self.requests_tx.clone(), self.ports_tx.clone())?;
+        self.connections.push(client_connection);
+        Ok(())
+    }
+
+    /// Get a channel for adding new ports with client connections.
+    pub fn get_port_channel(&self) -> mpsc::UnboundedSender<JsValue> {
+        self.ports_tx.clone()
+    }
+}
+
+/// Represents a one to one connection with a [`Client`] over a js object with port-like semantics.
+pub struct ServerConnection<Request, Response> {
+    _worker_join_handle: JoinHandle,
+    _worker_drop_guard: DropGuard,
+    _transferred_types: PhantomData<(Request, Response)>,
+}
+
+impl<Request, Response> ServerConnection<Request, Response>
+where
+    Request: Serialize + DeserializeOwned + 'static,
+    Response: Serialize + 'static,
+{
+    /// Start a new client connection, spinning up background thread for forwarding messages from
+    /// the callback
+    pub fn start(
+        port: JsValue,
+        request_tx: mpsc::UnboundedSender<(Request, oneshot::Sender<Response>)>,
+        ports_tx: mpsc::UnboundedSender<JsValue>,
+    ) -> Result<ServerConnection<Request, Response>> {
+        let cancellation_token = CancellationToken::new();
+        let mut worker =
+            ServerWorker::new(port, request_tx, ports_tx, cancellation_token.child_token())?;
+
+        let _worker_join_handle = spawn(async move {
+            if let Err(e) = worker.run().await {
+                error!("Serverworker stopped because of a fatal error: {e}");
+            }
+        });
+
+        Ok(ServerConnection::<Request, Response> {
+            _worker_join_handle,
+            _worker_drop_guard: cancellation_token.drop_guard(),
+            _transferred_types: PhantomData,
+        })
+    }
+}
+
+struct ServerWorker<Request: Serialize, Response> {
+    /// Port over which communication takes place
+    port: Port,
+    /// Queued requests from the onmessage callback
+    incoming_requests: mpsc::UnboundedReceiver<MultiplexMessage<Request>>,
+    /// Futures waiting for completion to be send as responses
+    pending_responses_map: FuturesUnordered<LocalBoxFuture<'static, (MessageId, Option<Response>)>>,
+    /// Channel to send requests and response senders over
+    request_tx: mpsc::UnboundedSender<(Request, oneshot::Sender<Response>)>,
+    /// Cancellation token to stop the worker
+    cancellation_token: CancellationToken,
+}
+
+impl<Request, Response> ServerWorker<Request, Response>
+where
+    Request: Serialize + DeserializeOwned + 'static,
+    Response: Serialize + 'static,
+{
+    fn new(
+        port: JsValue,
+        request_tx: mpsc::UnboundedSender<(Request, oneshot::Sender<Response>)>,
+        port_queue: mpsc::UnboundedSender<JsValue>,
+        cancellation_token: CancellationToken,
+    ) -> Result<ServerWorker<Request, Response>> {
+        let (incoming_requests_tx, incoming_requests) = mpsc::unbounded_channel();
+
+        let port = Port::new_with_channels(port, incoming_requests_tx, Some(port_queue))?;
+
+        Ok(ServerWorker {
+            port,
+            incoming_requests,
+            pending_responses_map: Default::default(),
+            request_tx,
+            cancellation_token,
+        })
+    }
+
+    async fn run(&mut self) -> Result<()> {
+        loop {
+            select! {
+                _ = self.cancellation_token.cancelled() => {
+                    return Ok(())
+                }
+                msg = self.incoming_requests.recv() => {
+                    let Some(MultiplexMessage {id, payload, }) = msg else {
+                        return Err(Error::new("Incoming message channel closed, should not happen"));
+                    };
+
+                    match payload {
+                        Some(request) =>
+                            self.handle_incoming_request(id, request)?,
+                        None => {
+                            warn!("Received message with empty payload");
+                            // send back empty message back, in case somebody's waiting
+                            self.handle_outgoing_response(id, None)?;
+                        }
+                    }
+                }
+                res = self.pending_responses_map.next(), if !self.pending_responses_map.is_empty() => {
+                    let Some((id, response) ) = res else {
+                    return Err(Error::new("Responses channel closed, should not happen"));
+                };
+                    self.handle_outgoing_response(id, response)?;
+                }
+            }
+        }
+    }
+
+    fn handle_incoming_request(&mut self, id: MessageId, request: Request) -> Result<()> {
+        let (response_tx, response_rx) = oneshot::channel();
+
+        self.request_tx
+            .send((request, response_tx))
+            .context("forwarding received request failed, no receiver waiting")?;
+
+        let tagged_response = response_rx.map(move |r| (id, r.ok())).boxed_local();
+
+        self.pending_responses_map.push(tagged_response);
+
+        Ok(())
+    }
+
+    fn handle_outgoing_response(&mut self, id: MessageId, payload: Option<Response>) -> Result<()> {
+        let message = MultiplexMessage { id, payload };
+
+        self.port
+            .send(&message)
+            .context("failed to send outgoing response ")?;
+
+        Ok(())
+    }
+}
