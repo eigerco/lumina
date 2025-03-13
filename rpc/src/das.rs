5c5e07b9e56533c30cd9b1b556317ee27ce9013f
Yakup Altay <yakup.altay@boun.edu.tr>
2025-03-13 23:31:50 +0300

    fix(das): update documentation for SamplingStats response type and adjust serde naming

--- rpc/src/das.rs
+++ rpc/src/das.rs
@@ -16,8 +16,9 @@ pub struct DasWorker {
     pub to: u64,
 }
 
-/// Response type for [`DasClient::SamplingStats`].
+/// Response type for [`DasClient::das_sampling_stats`].
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
 pub struct SamplingStats {
     /// The height of the most recently sampled block in the chain.
     pub head_of_sampled_chain: u64,
