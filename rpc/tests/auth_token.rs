9cd3b19e34416d59876c0ba469c2459d3cabc0a4
Yakup Altay <yakup.altay@boun.edu.tr>
2025-04-03 16:51:45 +0300

    refactor(tests): format code for readability in auth_token.rs

--- rpc/tests/auth_token.rs
+++ rpc/tests/auth_token.rs
@@ -17,8 +17,8 @@ async fn blob_submit_using_bridge_node() {
     let data = random_bytes(5);
     let blob = Blob::new(namespace, data, AppVersion::V2).unwrap();
 
-    // AuthLevel::Skip
-    let unauthorized_client = new_test_client_with_url(AuthLevel::Skip, CELESTIA_BRIDGE_RPC_URL).await;
+    let unauthorized_client =
+        new_test_client_with_url(AuthLevel::Skip, CELESTIA_BRIDGE_RPC_URL).await;
     assert!(unauthorized_client.is_err());
 
     for auth_level in [AuthLevel::Read, AuthLevel::Write, AuthLevel::Admin] {
