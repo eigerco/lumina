d5d752befe098d69a1690acab5f93ef7b0abca4b
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2025-04-16 12:48:50 +0200

    fix gradle, update folder struct to mirror ios
    Signed-off-by: Mikołaj Florkiewicz <mikolaj@florkiewicz.me>

--- examples/android/LuminaDemo/app/build.gradle.kts
+++ examples/android/LuminaDemo/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "co.eiger.luminademo"
-        minSdk = 27
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
