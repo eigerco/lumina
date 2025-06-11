27b5583d59f0ab6063336ea67a28aae51aca2d77
Yiannis Marangos <psyberbits@gmail.com>
2025-06-11 11:35:20 +0300

    Update node-uniffi/src/types/event.rs
    Co-authored-by: Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
    Signed-off-by: Yiannis Marangos <psyberbits@gmail.com>

--- node-uniffi/src/types/event.rs
+++ node-uniffi/src/types/event.rs
@@ -139,7 +139,7 @@ pub enum NodeEvent {
     PrunedHeaders {
         /// Start of the range.
         from_height: u64,
-        /// End of the range (included).
+        /// End of the range (inclusive).
         to_height: u64,
     },
     /// Pruning fatal error.
