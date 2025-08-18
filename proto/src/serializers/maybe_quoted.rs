4bea2a732c60fee51c780e8ccfd9e6644be4d22d
zvolin <mac.zwolinski@gmail.com>
2025-08-18 16:10:43 +0200

    fix(proto,types)!: fix binary serialization of maybe_quoted

--- proto/src/serializers/maybe_quoted.rs
+++ proto/src/serializers/maybe_quoted.rs
@@ -5,11 +5,36 @@
 use std::fmt::Display;
 use std::str::FromStr;
 
-use serde::{Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 use crate::serializers::cow_str::CowStr;
 
-pub use crate::serializers::from_str::serialize;
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum MaybeQuoted<'a, T> {
+    Direct(T),
+    #[serde(borrow)]
+    Quoted(CowStr<'a>),
+}
+
+#[derive(Serialize, Deserialize)]
+enum MaybeQuotedTagged<T> {
+    Direct(T),
+    Quoted(String),
+}
+
+/// Serialize from T into string
+pub fn serialize<S, T>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+    T: Serialize + Display,
+{
+    if serializer.is_human_readable() {
+        value.to_string().serialize(serializer)
+    } else {
+        MaybeQuotedTagged::<T>::Quoted(value.to_string()).serialize(serializer)
+    }
+}
 
 /// Deserialize T directly or from string
 pub fn deserialize<'de, D, T>(deserializer: D) -> Result<T, D::Error>
@@ -18,16 +43,15 @@ where
     T: Deserialize<'de> + FromStr,
     <T as FromStr>::Err: Display,
 {
-    #[derive(Deserialize)]
-    #[serde(untagged)]
-    enum MaybeQuoted<'a, T> {
-        Direct(T),
-        #[serde(borrow)]
-        Quoted(CowStr<'a>),
-    }
-
-    match MaybeQuoted::deserialize(deserializer)? {
-        MaybeQuoted::Direct(t) => Ok(t),
-        MaybeQuoted::Quoted(s) => s.parse().map_err(serde::de::Error::custom),
+    if deserializer.is_human_readable() {
+        match MaybeQuoted::deserialize(deserializer)? {
+            MaybeQuoted::Direct(t) => Ok(t),
+            MaybeQuoted::Quoted(s) => s.parse().map_err(serde::de::Error::custom),
+        }
+    } else {
+        match MaybeQuotedTagged::deserialize(deserializer)? {
+            MaybeQuotedTagged::Direct(t) => Ok(t),
+            MaybeQuotedTagged::Quoted(s) => s.parse().map_err(serde::de::Error::custom),
+        }
     }
 }
